634e5b69f7a3c06d48d856924c378404db030843
Nikhil Kumar <nikhilkumar@Nikhils-MacBook-Air-3.local>
2025-10-15 08:48:36 +0530

    Fix MongoDB connection issue - Remove bufferCommands:false for Vercel compatibility

--- api/index.js
+++ api/index.js
@@ -42,7 +42,6 @@ mongoose.connect(process.env.MONGODB_URI, {
   maxPoolSize: 10,        // Maintain up to 10 socket connections
   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  bufferCommands: false,  // Disable mongoose buffering
   maxIdleTimeMS: 30000,   // Close connections after 30 seconds of inactivity
   family: 4               // Use IPv4, skip trying IPv6
 })
